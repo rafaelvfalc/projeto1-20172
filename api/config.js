6182c7a9638b80afd46fd954009f0cbf647f675e
Samantha Monteiro <samanthakem@gmail.com>
2017-11-19 15:59:11 -0300

    Changing DB config

--- api/config.js
+++ api/config.js
@@ -4,6 +4,6 @@ config.jwt = {};
 config.jwt.secret = '7x0jhxt"9(thpX6';
 
 config.mongodb = {};
-config.mongodb.url = process.env.MONGO_URL || 'mongodb://localhost/buzzticketdb';
+config.mongodb.url = process.env.MONGO_URL || 'mongodb://buzzticketdb:buzzticketdb@ds113606.mlab.com:13606/buzzticketdb';
 
 module.exports = config;

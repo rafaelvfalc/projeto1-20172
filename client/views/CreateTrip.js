2b06ac99088b6c0ac5285c160415cdc4b161279d
JuliePessoa <julie.pessoa@ccc.ufcg.edu.br>
2017-11-27 23:41:04 -0300

    adding creating trip view

---
+++ client/views/CreateTrip.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  AsyncStorage,
+  ScrollView,
+  TouchableHighlight
+} from 'react-native';
+
+  const t = require('tcomb-form-native')
+
+  const Form = t.form.Form
+
+  const TripPlan = t.struct({
+    origem: t.String,
+    destino: t.String,
+    duracao: t.Number,
+    modeloOnibus: t.String,
+    mapa: t.String,
+    cadeiras: t.String,
+    onibus: t.String,
+    dia: t.Number,
+    hora: t.String,
+    semanal: t.Boolean,
+    feriado: t.Boolean,
+    ativado: t.Boolean
+  })
+
+  class CreateTrip extends Component {
+
+    constructor(props) {
+      super(props)
+      this.state = {
+        value: {
+          origem: '',
+          destino: '',
+          duracao: 0,
+          modeloOnibus: '',
+          mapa: '',
+          cadeiras: '',
+          onibus: '',
+          dia: 0,
+          hora: '',
+          semanal: true,
+          feriado: false,
+          ativado: true
+        }
+      }
+    }
+
+    componentWillUnmount() {
+      this.setState = {
+        value: {
+          origem: '',
+          destino: '',
+          duracao: 0,
+          modeloOnibus: '',
+          mapa: '',
+          cadeiras: '',
+          onibus: '',
+          dia: 0,
+          hora: '',
+          semanal: true,
+          feriado: false,
+          ativado: true
+        }
+      }
+    }
+
+    _onChange = (value) => {
+      this.setState({
+        value
+      })
+    }
+    _handleAdd = () => {
+      const value = this.refs.form.getValue();
+      // If the form is valid...
+      if (value) {
+        const data = {
+          _trip_plan: {
+            route: {
+              origin: value.origem,
+              destination: value.destination,
+              duration: value.duracao
+            },
+            bus: {
+              model: value.modeloOnibus,
+              seat_map: value.mapa,
+              seats: [value.cadeiras]
+            },
+            day: value.dia,
+            hour: value.hora,
+            weekly: value.semanal,
+            holiday: value.feriado,
+            enabled: value.ativado
+          },
+          date: new Date()
+        }
+        // Serialize and post the data
+        alert(data)
+        const json = JSON.stringify(data)
+        fetch('http://aqueous-oasis-59499.herokuapp.com/trips', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+          },
+          body: json
+        })
+        .then((response) => response.json())
+        .then((res) => {
+          if (res.error) {
+            alert(res.error)
+          } else {
+            AsyncStorage.setItem('jwt', res.token)
+            alert(`Success! The trip was created.`)
+            // Redirect to home screen
+            // this.props.navigator.pop()
+          }
+        })
+        .catch((err) => {
+          alert('There was an error creating a trip' + err);
+        })
+        .done()
+      } else {
+        // Form validation error
+        alert('Please fix the errors listed and try again.')
+      }
+    }
+
+    render() {
+      return (
+        <ScrollView style={styles.container}>
+          <Form
+            ref='form'
+            type={TripPlan}
+            value={this.state.value}
+            onChange={this._onChange}
+          />
+          <TouchableHighlight onPress={this._handleAdd}>
+            <Text style={styles.button}>Criar</Text>
+          </TouchableHighlight>
+        </ScrollView>
+      )
+    }
+  };
+
+  var styles = StyleSheet.create({
+    container: {
+      padding: 20,
+      flex: 1,
+      flexDirection: 'column'
+    },
+    button: {
+      borderRadius: 4,
+      padding: 20,
+      textAlign: 'center',
+      marginBottom: 20,
+      color: '#fff',
+      backgroundColor: '#1e698d'
+    },
+    centering: {
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  })
+
+  module.exports = CreateTrip

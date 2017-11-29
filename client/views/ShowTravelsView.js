0bfbcbeead34cab41816848ab72a567fdf4f22f8
Rafael Falcao <rafael.falcao@ccc.ufcg.edu.br>
2017-11-29 17:00:02 -0300

    progress in comunication between views

--- client/views/ShowTravelsView.js
+++ client/views/ShowTravelsView.js
@@ -18,15 +18,28 @@ import {
 class ShowTravelsView extends Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      res : ''
+    };
   }
 
-// https://my.api.mockaroo.com/travels.json?key=ff8bc390 -> url do mock JSON
-// http://www.mockaroo.com/virtual_services/913 -> info
+// https://my.api.mockaroo.com/users.json?key=ff8bc390 -> url do mock JSON
+// http://www.mockaroo.com/virtual_services/922 -> info
 
-  _test = () => {
-    alert(this.props.navigation.state.params._from)
+    _getTravelsFromApi = (props) => {
+      return fetch('https://my.api.mockaroo.com/users.json?key=8ee6d780')
+      .then((response) => response.json())
+      .then((responseJson) => {   
+        alert(JSON.stringify(responseJson))   
+        this.setState({ res : responseJson})
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
+  //_test = () => {
+  //  alert(this.props.navigation.state.params._from)
+  //}
 
   render() {
     return (
@@ -34,7 +47,7 @@ class ShowTravelsView extends Component {
         <Text style={{fontSize:20}}>
         Show Travels!
        </Text>
-      <TouchableHighlight onPress={this._test}>
+      <TouchableHighlight onPress={() => this._getTravelsFromApi(this.props)}>
         <Text style={styles.button}>Testing sending parameters...</Text>
       </TouchableHighlight>
       </View>

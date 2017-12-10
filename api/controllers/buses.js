a373414cd8c766ebd2d0404d1790fd3a7de67215
silveiraluiza <luiza.silveira@ccc.ufcg.edu.br>
2017-12-10 19:09:25 -0200

    minor fixes

--- api/controllers/buses.js
+++ api/controllers/buses.js
@@ -25,6 +25,8 @@ exports.create_bus = function (req, res, next) {
     })(req, res, next);
 }
 
+// query = dbSchemas.SomeValue.find({}).select('name -_id');
+
 exports.list_all_buses = function (req, res, next) {
   passport.authenticate('jwt', function (err, user, info) {
     if (err) {
@@ -35,6 +37,24 @@ exports.list_all_buses = function (req, res, next) {
     }
     if (user) {
       Bus.find({}, function(err, buses) {
+        if (err)
+          res.send(err);
+        res.json(buses);
+      });
+    }
+  })(req, res, next);
+}
+
+simple_search = function (req, res, next, query) {
+  passport.authenticate('jwt', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({ error: 'Invalid credentials.' });
+    }
+    if (user) {
+      Bus.find(query, function(err, buses) {
         if (err)
           res.send(err);
         res.json(buses);
--- api/routes/trips.js
+++ api/routes/trips.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/trips')
+var controllerR = require('../controllers/routes')
+var controllerP = require('../controllers/trip_plans')
+
+
+
 
 router.post('/', controller.create_trip);
 router.get('/', controller.list_all_trips);

b5905110848c82e7db619eb9aa78b2d18a03769c
Rafael Falcao <rafael.falcao@ccc.ufcg.edu.br>
2017-12-12 12:13:04 -0300

    adding connection between search component and show searched trips

--- client/App/Containers/TripScreen.js
+++ client/App/Containers/TripScreen.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import { FlatList, StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View, RefreshControl, ActivityIndicator } from "react-native";
+
+import Timeline from 'react-native-timeline-listview'
 
 import styles from './Styles/TripScreenStyles'
 
@@ -15,8 +17,14 @@ export default class TripScreen extends Component {
     //alert(this.props.navigation.state.params._depart)
     //alert(this.props.navigation.state.params._return)
 
+    this.data = []
+
+    this.tst = []
+    
     this.state = {
-      data: []
+      isRefreshing: false,
+      waiting: false,
+      data: this.data
     }
 
   }
@@ -25,33 +33,82 @@ export default class TripScreen extends Component {
     this.fetchData();
   }
 
-  fetchData = async (props) => {
-    const response = await fetch("https://my.api.mockaroo.com/travel.json?key=8ee6d780");
-    const json = await response.json();
-    var from = this.from
-    var to = this.to
-    var depart_date = this.depart
-    var return_date = this.return
-    var trips_founded = []
-    json.forEach(function(trip) { 
-      if (trip.from == from && trip.to == to && trip.depart_date == depart_date && trip.return_date == return_date) {
-        alert(JSON.stringify(trip));
-      }
-    });
-    this.setState({ data: trips_founded  });
-  };
+  fetchData(){
+    fetch("https://my.api.mockaroo.com/travel.json?key=8ee6d780")
+    .then((response) => response.json())
+    .then((responseData) => {
+      var from = this.from
+      var to = this.to
+      var depart_date = this.depart
+      var return_date = this.return
+      var trips = []
+      responseData.forEach(function(trip) { 
+       if (trip.origem == from && trip.destino == to && trip.dia == depart_date) {
+        trips.push({time:trip.hora.toString(), title:trip.origem.toString() + " - " + trip.destino.toString(), description: "Viagem de duração de " + trip.duracao.toString() + " h , ônibus da " + trip.onibus + " modelo " + trip.modeloOnibus + ", com " + trip.cadeiras + " vagas disponíveis."})
+          //alert(JSON.stringify({time:trip.hora.toString(), title:trip.origem.toString() + " - " + trip.destino.toString(), description: "Viagem de duração de " + trip.duracao.toString()}))
+          //data.push({time:trip.hora.toString(), title:trip.origem.toString() + " - " + trip.destino.toString(), description: "Viagem de duração de " + trip.duracao.toString()})
+        }
+      });
+      this.data = trips
+      this.setState((state) => ({ data: trips}));      
+      //this.setState({
+      //  dataSource: this.state.dataSource.cloneWithRows(responseData.results),
+      //  loaded: true,
+      //});
+    })
+    .done();
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          data={this.state.data}
-          keyExtractor={(x, i) => i}
-          renderItem={({ item }) =>
-            <Text>
-            </Text>}
-        />
-      </View>
-    );
   }
+
+  onEndReached = () => {
+    if (!this.state.waiting) {
+      this.setState({waiting: true});
+      //fetch and concat data
+      setTimeout(() => {
+        //refresh to initial data
+        var data = this.state.data.concat([])
+
+        this.setState({
+          waiting: false,
+          data: data,
+        });
+      }, 2000);
+    }
+  }
+
+  renderFooter = () => {
+    if (this.state.waiting) {
+      return <ActivityIndicator />;
+    } else {
+     return <Text>~</Text>;
+   }
+ }
+
+ render() {
+  //'rgb(45,156,219)'
+  return (
+  <View style={styles.container}>
+  <Timeline
+  style={styles.list}
+  data={this.state.data}
+  circleSize={20}
+  circleColor='white'
+  lineColor='white'
+  timeContainerStyle={{minWidth:52, marginTop: -5}}
+  timeStyle={{textAlign: 'center', backgroundColor:'#1e698d', color:'white', padding:5, borderRadius:13}}
+  descriptionStyle={{color:'black'}}
+  options={{
+    style:{paddingTop:5},
+    refreshControl: (
+    <RefreshControl
+    refreshing={this.state.isRefreshing}
+    onRefresh={this.onRefresh}
+    />
+    ),
+    renderFooter: this.renderFooter,
+  }}
+  />
+  </View>
+  );
+}
 }
